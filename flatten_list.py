046ea38373b7eb151c8ec006526b49b247515222
Cian O'Driscoll <cian.o-driscoll@hp.com>
2016-11-29 13:36:38 +0000

    Add test case for empty list
    Add check for empty list and test.
    Also add test case for null list.
    Correct input_fail list due to passing input_err_1 twice..
    Also some pep8 compliance issue.
    Removed unused variable flattened.

--- flatten_list.py
+++ flatten_list.py
@@ -3,11 +3,13 @@ import sys
 input_pass_1 = [[1, 2, [3]], 4]
 input_pass_2 = [[1, 2, [3, 4, [5, 6]]], 7]
 input_pass_3 = [[1, 2, [3, 4, [5, 6, [7, 8, 9]]]], 10]
-input_err_1 = 5
-input_err_2 = [[1, 2, [3, 4, [5, 6, [7, 8, 'X']]]], 9]
+input_err_1 = None
+input_err_2 = []
+input_err_3 = 5
+input_err_4 = [[1, 2, [3, 4, [5, 6, [7, 8, 'X']]]], 9]
 
 inputs_pass = [input_pass_1, input_pass_2, input_pass_3]
-inputs_fail = [input_err_1, input_err_1]
+inputs_fail = [input_err_1, input_err_2, input_err_3, input_err_4]
 
 
 def flatten_list(input, output):
@@ -17,6 +19,8 @@ def flatten_list(input, output):
     '''
     if not isinstance(input, list):
         raise ValueError('Expected list, found %s' % type(input))
+    elif not input:
+        raise ValueError('List empty, list (%s)' % input)
 
     for item in input:
         if isinstance(item, list):
@@ -36,7 +40,7 @@ def run_tests():
     '''
     for input in inputs_pass:
         output = []
-        flattened = flatten_list(input, output)
+        flatten_list(input, output)
 
     err_count = 0
     for input in inputs_fail:
@@ -44,11 +48,11 @@ def run_tests():
         try:
             flattened = flatten_list(input, output)
         except ValueError:
-            err_count+=1
+            err_count += 1
 
     if err_count != len(inputs_fail):
         print "Did not hit error count, expected (%s) errors got (%s)" \
-          % (len(inputs_fail), err_count)
+            % (len(inputs_fail), err_count)
         sys.exit(1)
 
     print "Pass!!!"
