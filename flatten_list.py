f55ae723f1270cc8ac1eeceab6a8b694b8874835
Cian O'Driscoll <cian.o-driscoll@hp.com>
2016-11-29 09:59:56 +0000

    Adds a function to flatten a list of lists.
    Uses recursion in flatten_list function to flatten the
    list.
    Add some unit tests for the flatten_list.
    Three pass cases and two failure cases.

---
+++ flatten_list.py
@@ -0,0 +1,57 @@
+import sys
+
+input_pass_1 = [[1, 2, [3]], 4]
+input_pass_2 = [[1, 2, [3, 4, [5, 6]]], 7]
+input_pass_3 = [[1, 2, [3, 4, [5, 6, [7, 8, 9]]]], 10]
+input_err_1 = 5
+input_err_2 = [[1, 2, [3, 4, [5, 6, [7, 8, 'X']]]], 9]
+
+inputs_pass = [input_pass_1, input_pass_2, input_pass_3]
+inputs_fail = [input_err_1, input_err_1]
+
+
+def flatten_list(input, output):
+    '''
+    Takes a input list of list and flattens into one list.
+    Uses recursion to achieve this
+    '''
+    if not isinstance(input, list):
+        raise ValueError('Expected list, found %s' % type(input))
+
+    for item in input:
+        if isinstance(item, list):
+            flatten_list(item, output)
+        elif isinstance(item, int):
+            output.append(item)
+        else:
+            raise ValueError('Expected int, found %s' % type(item))
+
+    return output
+
+
+def run_tests():
+    '''
+    Tests flatten_list function. Runs a list of tests
+    that expected to pass and a set expected to fail
+    '''
+    for input in inputs_pass:
+        output = []
+        flattened = flatten_list(input, output)
+
+    err_count = 0
+    for input in inputs_fail:
+        output = []
+        try:
+            flattened = flatten_list(input, output)
+        except ValueError:
+            err_count+=1
+
+    if err_count != len(inputs_fail):
+        print "Did not hit error count, expected (%s) errors got (%s)" \
+          % (len(inputs_fail), err_count)
+        sys.exit(1)
+
+    print "Pass!!!"
+
+if __name__ == "__main__":
+    run_tests()
